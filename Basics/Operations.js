34bdb331f9a404ce46862f4cab0d372837534241
Jeel Donga <jeeldonga18@gmail.com>
2025-06-19 19:03:43 +0000

    Operation done

---
+++ Basics/Operations.js
@@ -0,0 +1,34 @@
+// Operation means the Two or more Operands are the connected through an operator(s).
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Jay Shree "
+let str2 = "Ram"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);       // 12 
+console.log(1 + "2");       // 12
+console.log("1" + 2 + 2);   // 122
+console.log(1 + 2 + "2");   // 32
+// To avoid the Confusion refer the ECMAScript Document : type Conversion 
+
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3;
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+// To Learn about prefix and postfix operator refer the MDN Document 
