103266946b163b6cad69ae0f5a3533c45400ecc3
Jeel Donga <jeeldonga18@gmail.com>
2025-06-21 18:29:25 +0000

    Day Start by the learning of numbers and maths

---
+++ Basics/09_numbers_and_maths.js
@@ -0,0 +1,8 @@
+const score = 400  // way 1 : to declare the number type variable
+console.log(score);
+
+// you can also make the variable by specify its type 
+// way 2 : to declare the number type variable specifically
+
+const balance = new Number(100);
+console.log(balance);
